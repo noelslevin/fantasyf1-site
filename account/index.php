10a36e6bc98513bb03895fff780cae687248827e
Noel Slevin <noelslevin@gmail.com>
2014-05-17 12:52:09 +0100

    Sessions
    Fixed an issue where session_start() was declared twice on user profile
    pages.

--- account/index.php
+++ account/index.php
@@ -6,7 +6,6 @@ if(isset($_GET['page'])) { $page = $_GET['page']; } else { $page= NULL; }
 
 if (isset($_SESSION['user_id'])) {
 	include 'profile.php';
-	
 }
 else {
 	header ("Location:  http://" . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . "/login.php");
--- account/profile.php
+++ account/profile.php
@@ -1,6 +1,5 @@
 <?php
 
-session_start();
 $error = NULL;
 $output = NULL;
 
