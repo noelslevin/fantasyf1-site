f14844eb6df39d0336674a51bf4986a747ebec93
Noel Slevin <noelslevin@gmail.com>
2014-05-17 12:11:51 +0100

    Standings
    Made adjustments to SQL queries for standings as I discovered some views
    don't sort properly when rebuilt. Therefore, sorting is included in the
    SQL queries.

--- standings/2013.php
+++ standings/2013.php
@@ -1,10 +1,11 @@
 <h1>2013 FantasyF1 Standings</h1>
 <h2>Drivers' Standings</h2>
 <?php
+$season = '2013';
 echo "<table>\n";
 echo "<thead><tr><th class=position>Position</th><th class=name>Name</th><th class=team>Team</th><th class=points>Points</th><th class=total>(Score)</th></tr></thead>\n<tbody>\n";
 $num = 0;
-$sql="SELECT * FROM view_ff1individualstandingsbyyear WHERE season = '2013'";
+$sql="SELECT * FROM view_ff1individualstandingsbyyear WHERE season = '$season' ORDER BY points DESC, total DESC, username ASC";
 foreach ($dbh->query($sql) as $row) {
 	$num++;
 	$username = $row['username'];
@@ -21,7 +22,7 @@ echo "</tbody>\n</table>\n\n";
 echo "<table>\n";
 echo "<thead><tr><th>Position</th><th>Team</th><th>Points</th><th>(Score)</th></tr></thead>\n<tbody>\n\n";
 $num = 0;
-$sql="SELECT * FROM view_ff1teamstandingsbyyear WHERE season = '2013'";
+$sql="SELECT * FROM view_ff1teamstandingsbyyear WHERE season = '$season' ORDER BY points DESC, total DESC, team ASC";
 foreach ($dbh->query($sql) as $row) {
 	$num++;
 	$team = $row['team'];
