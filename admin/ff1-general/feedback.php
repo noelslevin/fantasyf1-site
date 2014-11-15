40993a8fa55ab4063594b53612ecd92ac1c39700
Noel Slevin <noelslevin@gmail.com>
2014-11-15 21:31:30 +0000

    User feedback
    Added a page to display user feedback submitted through the site.

---
+++ admin/ff1-general/feedback.php
@@ -0,0 +1,32 @@
+<h2>User feedback</h2>
+
+<style>
+  table, th, td {
+    border: 1px solid #000;
+    border-collapse: collapse;
+  }
+  th, td {
+    padding: 5px;
+  }
+</style>
+
+<?php
+
+$sql = "SELECT * FROM fantasyusers WHERE registered = '1'";
+$query = $dbh->prepare($sql);
+$query->execute();
+if ($query->rowCount() > 0) {
+  $result = $query->fetchAll(PDO::FETCH_OBJ);
+  echo "<table>\n";
+  echo "<thead><tr><th>Name</th><th>Bugs</th><th>Features</th><th>2015</th></tr></thead>\n";
+  foreach ($result as $row) {
+    echo "<tr><td>".$row->username."</td><td>".$row->bugs."</td><td>".$row->features."</td><td>".$row->nextseason."</td></tr>\n";
+  }
+  echo "<tbody>\n";
+  echo "</tbody></table>\n";
+}
+else {
+  echo "<p>No data found.</p>";
+}
+
+?>
--- admin/index.php
+++ admin/index.php
@@ -32,6 +32,7 @@ a {
 <a href=ff1-general/index.php?page=ff1teams>FF1 Teams</a>
 <a href=ff1-general/index.php?page=ff1teamstoseasons>FF1 Teams to Seasons</a>
 <a href=ff1-general/index.php?page=ff1userstoseasons>FF1 Users to Seasons</a>
+  <a href=ff1-general/index.php?page=feedback>User feedback</a>
 </div>
 
 <div class=block>
