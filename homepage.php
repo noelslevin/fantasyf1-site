38c3c01433662af5f5e972ed206ef1faeffbea44
Noel Slevin <noelslevin@gmail.com>
2015-01-01 23:30:35 +0000

    Updated homepage
    Updated homepage. Now looks like it has a bit more content…

--- homepage.php
+++ homepage.php
@@ -1,14 +1,44 @@
-<div class="small-12 medium-4 columns">
-  <h2>About FantasyF1</h1>
-  <p>Formula One is great, but it can be a bit dull sometimes. It's fun to have a little excitement riding on the race results. FantasyF1 provides exactly that! <a href=/about/>Find out more</a>, and be sure to check out the <a href=/rules/>rules</a>…</p>
-</div>
+<div class="homepage">
+  <div class="small-12 medium-4 columns homepage-panel">
+    <a href="/standings/">
+      <h2>Standings</h2>
+      <p>See the latest FantasyF1 individual and team standings.</p>
+    </a>
+  </div>
 
-<div class="small-12 medium-4 columns">
-  <h1>How Do I Start?</h1>
-  <p>It's great fun to look at people's <a href=/profiles/>profiles</a>, recent <a href=/results/> results</a> and the latest <a href=/standings/>standings</a>. There's even some fun <a href=/statistics/>statistics</a> to pore over if you must. Get a feel for things, but most importantly, sign up and play!</p>
-</div>
+  <div class="small-12 medium-4 columns homepage-panel">
+    <a href="/statistics/">
+      <h2>Statistics</h2>
+      <p>Who's won the most races? Who's finished last the most? Who's won the most titles? Wonder no longer…</p>
+    </a>
+  </div>
 
-<div class="small-12 medium-4 columns">
-  <h1>Regular F1 News</h1>
-  <p><a href="http://www.f1minute.com">F1 Minute</a> is a fantastic site with short snippets of F1 news. If you're playing FantasyF1, you'll want to check the latest news to try and find an edge on everyone else. For more, there's also <a href="http://www.sidepodcast.com">Sidepodcast</a>.</p>
+  <div class="small-12 medium-4 columns homepage-panel">
+    <a href="/profiles/">
+      <h2>Profiles</h2>
+      <p>Check out individual users' recent race results and competition history. It's good to know your rivals.</p>
+    </a>
+  </div>
+  
+  <div class="small-12 medium-4 columns homepage-panel">
+    <a href="/rules/">
+    <h2>Rules</h2>
+    <p>The boring (but important) bit. Contains information about picks, scoring and more. Oh, and complaints. But you don't have any, right?</p>
+    </a>
+  </div>
+  
+  <div class="small-12 medium-4 columns homepage-panel">
+    <a href="/about/">
+      <h2>About</h2>
+      <p>Still not entirely sure what this site's all about? Hopefully all your questions will be answered here.</p>
+    </a>
+  </div>
+
+  <div class="small-12 medium-4 columns homepage-panel">
+    <a href="/account/">
+      <h2>Account</h2>
+      <p>If you're registered to play FantasyF1, this is your route in to make your picks.</p>
+    </a>
+  </div>
+
 </div>
