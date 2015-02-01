0b34d6d1a9377d70c4076739343588c8395cdfd4
Noel Slevin <noelslevin@gmail.com>
2015-02-01 22:01:36 +0000

    Redundant stylesheet
    Removed a redundant and now removed stylesheet from the backend.

--- account/includes/header.php
+++ account/includes/header.php
@@ -12,7 +12,6 @@ if ($_SERVER['PHP_SELF'] == "/account/check-picks.php") {
 <meta name=viewport content="width=device-width,initial-scale=1,maximum-scale=1">
 <title>Your Account | FantasyF1 | Noelinho.org</title>
 <link rel="apple-touch-icon" href="/assets/img/icon.png">
-<link rel="stylesheet" href="/assets/foundation.min.css" />
 <link rel="stylesheet" href="/assets/css/main.min.css" />
 <meta name=description content="The (not quite) world-famous Noelinho.org FantasyF1 game">
 </head>
