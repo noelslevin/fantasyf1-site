05e08e0e0293d1359d4bd73fabe8bf0d4333f455
Noel Slevin <noelslevin@gmail.com>
2017-03-19 22:18:42 +0000

    Updated terms
    Updated terms, including reference to storing data in Mailchimp.

--- account/terms.php
+++ account/terms.php
@@ -10,11 +10,11 @@ if ($sql->rowCount() == 1) {
 		echo "<div class=panel>";
 		echo "<p>You must agree to the site terms below to continue.</p>\n";
 		echo "<ol>\n";
-		echo "<li>By creating a FantasyF1 account, you are entitled to participate in the FantasyF1 competition. In order to administer the competition, you agree to the storing of your email address, which is linked to your username.</li>\n";
-		echo "<li>You agree the site owner may use your email address to contact you about the FantasyF1 competition, including (but not limited to) information about race results, picks deadlines and site developments. Your email address will not be passed on to any third party.</li>\n";
+		echo "<li>By creating a FantasyF1 account, you are entitled to participate in the FantasyF1 competition. By participating in FantasyF1, you agree the site can store your email address, username and password.</li>\n";
+		echo "<li>You agree the site owner may use your email address to contact you about FantasyF1, including (but not limited to) information about race results, picks deadlines and site developments. For this purpose, your email address may also be stored in Mailchimp.</li>\n";
 		echo "<li>If you wish not to participate in future seasons, you will be given the opportunity to opt out and have your email address removed, but your public user data (e.g. race and season results) will still be available on the site.</li>\n";
-		echo "<li>Whilst every reasonable effort is made to make the site secure, it is always possible for malicious attacks to steal data. On this site, that is limited to your username, email address and password. Passwords on this site are salted and hashed using current and widely used standards. Your connection to this site is also encrypted. Therefore, your details are as safe as they can reasonably be. However, it is still strongly advised that you use a unique password for this site. In agreeing to these terms, you acknowledge this is solely your responsibility, and if you login details are compromised and used on other sites, the site owner is not responsible.</li>\n";
-		echo "<li>(Really important) You promise not to take this competition too seriously and let the site author win occasionally. </li>\n";
+		echo "<li>Whilst every effort is made to keep the site secure, including by using strongly encrypted connections, and passwords stored according to best practice principles, you acknowledge it is strongly recommended you use a password for your account which is different to those used on sensitive accounts, such as your personal email, banking, Tinder, etc.</li>\n";
+		echo "<li>(Very important) You promise not to take this competition too seriously and let the site author win occasionally.</li>\n";
 		echo "</ol>\n";
 		echo "<form action=\"".$_SERVER['PHP_SELF']."\" method=post>\n";
 		echo "<input type=checkbox name=terms value=1 required/>I agree to the site terms.<br>\n";
